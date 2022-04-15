ea056c48e2e7a8f639c5ed053fee52496f2af093
Negimaster <fandeslyc@hotmail.fr>
2022-04-16 00:08:56 +0200

    add: step7 tests and implementation

--- src/test/java/org/kata/compute/numbers/NumberComputeServiceTest.java
+++ src/test/java/org/kata/compute/numbers/NumberComputeServiceTest.java
@@ -420,4 +420,32 @@ class NumberComputeServiceTest {
             assertThat(computedNumbers).isEqualTo(54);
         }
     }
+
+    @Nested
+    class Step7Test {
+
+        @Test
+        void add_when_specify_multiple_char_delimiter_should_use_delimiter() {
+            // GIVEN
+            var numbers = "//***\n45***16***78";
+
+            // WHEN
+            var computedNumbers = numberComputeService.add(numbers);
+
+            // THEN
+            assertThat(computedNumbers).isEqualTo(139);
+        }
+
+        @Test
+        void add_when_specify_multiple_char_delimiter_should_use_delimiter_with_comma_and_linefeed() {
+            // GIVEN
+            var numbers = "//****\n45,16****78\n11";
+
+            // WHEN
+            var computedNumbers = numberComputeService.add(numbers);
+
+            // THEN
+            assertThat(computedNumbers).isEqualTo(150);
+        }
+    }
 }
